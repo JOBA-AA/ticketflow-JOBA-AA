fa614baceb51b07dc1bbc6fcfb2b06df481515cd
unknown <akinajayiemmanuel@gmail.com>
2026-05-16 23:58:32 +0100

    feat(compose): add nginx router and fix client API routing for local dev

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -2,17 +2,18 @@ import axios from 'axios';
 
 export default ({ req }) => {
   if (typeof window === 'undefined') {
-    // We are on the server
-
+    // We are on the server - use internal service routing
+    const baseURL = process.env.API_URL || 
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local';
+    
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL,
       headers: req.headers,
     });
   } else {
     // We must be on the browser
     return axios.create({
-      baseUrl: '/',
+      baseURL: '/',
     });
   }
 };
